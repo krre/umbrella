a7e427159c9145cb8a7e528ec2063c955e62425c
Vladimir Zarypov <krre31@gmail.com>
2025-10-30 17:27:05 +0200

    Cleanup `build.zig` file

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const exe = b.addExecutable(.{
+    const lib = b.addExecutable(.{
         .name = "lib",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/root.zig"),
@@ -25,35 +25,8 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    exe.entry = .disabled;
-    exe.out_filename = "../../web/lib.wasm";
+    lib.entry = .disabled;
+    lib.out_filename = "../../web/lib.wasm";
 
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    b.installArtifact(lib);
 }
