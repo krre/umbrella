7ce9b726a242b7dbf990c81c76864cfcd029e96b
Vladimir Zarypov <krre31@gmail.com>
2025-11-02 13:20:07 +0200

    Add using JavaScript `console.log()` function from Zig

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,7 @@
+extern fn console_log(message: [*]const u8, length: u8) void;
+
 export fn start() i32 {
+    const log = "Hello World!";
+    console_log(log, log.len);
     return 42;
 }
--- web/loader.js
+++ web/loader.js
@@ -1,8 +1,25 @@
+let instance;
+
+function console_log(location, size) {
+    var buffer = new Uint8Array(instance.exports.memory.buffer, location, size);
+    var decoder = new TextDecoder();
+    var string = decoder.decode(buffer);
+    console.log(string);
+}
+
 async function init() {
     const response = await fetch("lib.wasm");
     const bytes = await response.arrayBuffer();
 
-    const { instance } = await WebAssembly.instantiate(bytes, {});
+    const imports = {
+        env: {
+            memory: new WebAssembly.Memory({ initial: 1 }),
+            console_log: console_log,
+        }
+    };
+
+    const source = await WebAssembly.instantiate(bytes, imports);
+    instance = source.instance
     const exports = instance.exports;
     console.log("start() =", exports.start());
 }
