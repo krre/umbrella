355c8dcc50c71b965892675302fce25a18001eb4
Vladimir Zarypov <krre31@gmail.com>
2025-11-02 14:33:00 +0200

    Refactor `loader.js`

--- web/loader.js
+++ web/loader.js
@@ -1,27 +1,17 @@
-let instance;
-
-function console_log(location, size) {
-    var buffer = new Uint8Array(instance.exports.memory.buffer, location, size);
-    var decoder = new TextDecoder();
-    var string = decoder.decode(buffer);
-    console.log(string);
-}
+let wasm;
 
 async function init() {
-    const response = await fetch("lib.wasm");
-    const bytes = await response.arrayBuffer();
-
     const imports = {
         env: {
-            memory: new WebAssembly.Memory({ initial: 1 }),
-            console_log: console_log,
+            console_log: (ptr, len) => {
+                const bytes = new Uint8Array(wasm.instance.exports.memory.buffer, ptr, len);
+                console.log(new TextDecoder("utf8").decode(bytes));
+            }
         }
     };
 
-    const source = await WebAssembly.instantiate(bytes, imports);
-    instance = source.instance
-    const exports = instance.exports;
-    console.log("start() =", exports.start());
+    const wasm = await WebAssembly.instantiateStreaming(fetch("lib.wasm"), imports);
+    console.log("start() =", wasm.instance.exports.start());
 }
 
 init();
