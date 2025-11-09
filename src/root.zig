61ee24ab54c19ef25ed6d761ae716a1f8993e62f
Vladimir Zarypov <krre31@gmail.com>
2025-11-09 12:53:53 +0200

    Improve logging for application start steps

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,14 @@
 const std = @import("std");
+const console = @import("console.zig");
 const app = @import("app.zig");
 
 export fn start() void {
+    console.log("Application initing begin");
+
     const allocator = std.heap.wasm_allocator;
-    if (app.start(allocator)) |_| {} else |_| {}
+    if (app.start(allocator)) |_| {
+        console.log("Application started");
+    } else |_| {
+        console.log("Application start failure");
+    }
 }
