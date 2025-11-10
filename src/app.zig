ff270f661d3cc4708850a8b17fd9cb40c93fb6ed
Vladimir Zarypov <krre31@gmail.com>
2025-11-10 08:54:55 +0200

    Add WebGPU renderer

--- src/app.zig
+++ src/app.zig
@@ -1,21 +1,9 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const console = @import("console.zig");
-const gpu = @import("webgpu/gpu.zig");
-const adapter = @import("webgpu/adapter.zig");
-const device = @import("webgpu/device.zig");
-const canvas_context = @import("webgpu/canvas_context.zig");
+const renderer = @import("renderer.zig");
 
 pub fn start(allocator: Allocator) !void {
-    const webgpu = try gpu.Gpu.init(allocator);
-    try console.fmtLog(allocator, "GPU id: {d}", .{webgpu.id});
-
-    const gpu_adapter = try adapter.GpuAdapter.init(allocator);
-    try console.fmtLog(allocator, "GPUAdapter id: {d}", .{gpu_adapter.id});
-
-    const gpu_device = try device.GpuDevice.init(allocator);
-    try console.fmtLog(allocator, "GPUDevice id: {d}", .{gpu_device.id});
-
-    const gpu_canvas_context = try canvas_context.GpuCanvasContext.init(allocator);
-    try console.fmtLog(allocator, "GpuCanvasContext id: {d}", .{gpu_canvas_context.id});
+    const webgpu_renderer = try renderer.Renderer.init(allocator);
+    _ = webgpu_renderer;
 }
