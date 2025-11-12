e82e548a94e3bbca80e47459f4287a31966b2d3a
Vladimir Zarypov <krre31@gmail.com>
2025-11-12 17:20:48 +0200

    Add inner allocator to `console.fmtLog()` function

--- src/app.zig
+++ src/app.zig
@@ -6,8 +6,7 @@ const console = @import("console.zig");
 var renderer: Renderer = undefined;
 
 export fn resize(width: usize, height: usize) void {
-    const allocator = std.heap.wasm_allocator;
-    console.fmtLog(allocator, "resize {d}, {d}", .{ width, height }) catch {};
+    console.fmtLog("resize {d}, {d}", .{ width, height }) catch {};
 }
 
 pub fn start(allocator: Allocator) !void {
--- src/console.zig
+++ src/console.zig
@@ -7,8 +7,9 @@ pub fn log(message: []const u8) void {
     consoleLog(message.ptr, message.len);
 }
 
-pub fn fmtLog(allocator: Allocator, comptime fmt: []const u8, args: anytype) Allocator.Error!void {
+pub fn fmtLog(comptime fmt: []const u8, args: anytype) Allocator.Error!void {
+    const allocator = std.heap.wasm_allocator;
     const str = try std.fmt.allocPrint(allocator, fmt, args);
-    log(str);
     defer allocator.free(str);
+    log(str);
 }
