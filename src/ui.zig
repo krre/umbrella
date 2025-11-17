9552f9f1ae7f2de7b027798d461be9e65ae5d11c
Vladimir Zarypov <krre31@gmail.com>
2025-11-17 14:50:37 +0200

    Add 2D geometry types

--- src/ui.zig
+++ src/ui.zig
@@ -2,3 +2,4 @@ const UI3D = @import("ui/UI3D.zig");
 const Color = @import("ui/Color.zig");
 const Node = @import("ui/Node.zig");
 const Widget = @import("ui/Widget.zig");
+const dim2d = @import("2d.zig");
---
+++ src/ui/2d.zig
@@ -0,0 +1,6 @@
+pub const Pos2D = struct {
+    x: i32,
+    y: i32,
+};
+
+pub const Size2D = struct { width: u32, height: u32 };
