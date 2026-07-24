1ca730cbe295f16051f4519615ea398bf00e21eb
Vladimir Zarypov <krre31@gmail.com>
2026-07-24 09:29:27 +0300

    Add border to rectangle

--- src/ui/2d/Rectangle.zig
+++ src/ui/2d/Rectangle.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const Widget2D = @import("Widget2D.zig");
 const Color = @import("../Color.zig");
+const Border = @import("../Border.zig");
 
 const Rectangle = @This();
 
-color: Color,
+color: Color = Color.white,
+border: Border,
 widget2D: Widget2D,
 
 pub fn init(allocator: std.mem.Allocator) Rectangle {
-    return Rectangle{ .color = Color{}, .widget2D = Widget2D.init(allocator) };
+    return Rectangle{ .widget2D = Widget2D.init(allocator) };
 }
