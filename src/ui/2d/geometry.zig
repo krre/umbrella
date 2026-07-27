238df34caafc4a6c90830204ebe75a21b5769afd
Vladimir Zarypov <krre31@gmail.com>
2026-07-27 06:19:21 +0300

    Merge 2D and 3D geometry

--- src/ui/2d/geometry.zig
+++
@@ -1,9 +0,0 @@
-pub const Pos2D = struct {
-    x: i32,
-    y: i32,
-};
-
-pub const Size2D = struct {
-    width: u32,
-    height: u32,
-};
--- src/ui/geometry.zig
+++ src/ui/geometry.zig
@@ -1,10 +1,10 @@
-pub const Pos3D = struct {
+pub const Pos = struct {
     x: i32,
     y: i32,
     z: i32,
 };
 
-pub const Size3D = struct {
+pub const Size = struct {
     width: u32,
     height: u32,
     depth: u32,
