eab1edcbe9de8be4ad640121e1bde2e37b76d3ba
Vladimir Zarypov <krre31@gmail.com>
2026-07-31 08:03:56 +0300

    Remove unused import

--- src/ui/UI3D.zig
+++ src/ui/UI3D.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Renderer = @import("../gfx/Renderer.zig");
 const Widget = @import("widget/Widget.zig");
-const UmbrellaIDE = @import("../ide/UmbrellaIDE.zig");
 
 const UI3D = @This();
 
