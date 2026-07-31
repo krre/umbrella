48039c54b2ed523b3a760c50b85b86e909d5ede7
Vladimir Zarypov <krre31@gmail.com>
2026-07-31 08:22:25 +0300

    Add `Box` as root into `UmbrellaIDE`

--- src/ide/UmbrellaIDE.zig
+++ src/ide/UmbrellaIDE.zig
@@ -1,22 +1,14 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const Widget = @import("../ui/widget/Widget.zig");
+const Box = @import("../ui/widget/Box.zig");
 const UI3D = @import("../ui/UI3D.zig");
 
 const UmbrellaIDE = @This();
 
 pub var ui: UI3D = undefined;
-var root: Widget = undefined;
-
-const vtable = Widget.VTable{
-    .draw = &draw,
-};
+var root: Box = undefined;
 
 pub fn init(allocator: Allocator) void {
-    root = Widget.init(allocator, &vtable);
-    ui = UI3D.init(allocator, &root);
-}
-
-fn draw(self: *Widget) void {
-    _ = self;
+    root = Box.init(allocator);
+    ui = UI3D.init(allocator, &root.widget);
 }
