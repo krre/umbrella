61bc96bfb0a42f92ac17c7eb37a404fddbbae4d9
Vladimir Zarypov <krre31@gmail.com>
2026-08-01 19:22:15 +0300

    Add `Pos2D` and `Size2D` types

--- src/ui/geometry.zig
+++ src/ui/geometry.zig
@@ -1,3 +1,13 @@
+pub const Pos2D = struct {
+    x: i32,
+    y: i32,
+};
+
+pub const Size2D = struct {
+    width: u32,
+    height: u32,
+};
+
 pub const Pos3D = struct {
     x: f32,
     y: f32,
