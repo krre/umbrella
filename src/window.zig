42d13d2bc2151e7dba5bf80ef3cc53b9ec262535
Vladimir Zarypov <krre31@gmail.com>
2026-08-01 19:50:59 +0300

    Replace `width()` and `height()` functions by `size()` in `window.zig` module

--- src/window.zig
+++ src/window.zig
@@ -1,9 +1,6 @@
+const Size2D = @import("geometry.zig").Size2D;
 const js = @import("js.zig");
 
-pub fn width() u16 {
-    return js.windowWidth();
-}
-
-pub fn height() u16 {
-    return js.windowHeight();
+pub fn size() Size2D {
+    return .{ .width = js.windowWidth(), .height = js.windowHeight() };
 }
