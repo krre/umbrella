65e83a92cd3528839439abb28455f96e90c4b718
Vladimir Zarypov <krre31@gmail.com>
2026-08-02 14:48:05 +0300

    Fix variable name

--- src/root.zig
+++ src/root.zig
@@ -1,15 +1,14 @@
 const std = @import("std");
 const console = @import("console.zig");
-const Application = @import("ui/Application.zig");
-
 const js = @import("js.zig");
-const umbrella = @import("ide/Umbrella.zig");
+const Application = @import("ui/Application.zig");
+const Umbrella = @import("ide/Umbrella.zig");
 
 export fn start() void {
     console.log("Application initing", .{});
 
-    umbrella.init(std.heap.wasm_allocator);
-    js.app = &umbrella.app;
+    Umbrella.init(std.heap.wasm_allocator);
+    js.app = &Umbrella.app;
 
     if (js.app.start()) |_| {
         console.log("Application started", .{});
